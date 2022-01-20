27b28f90e04e3a658cad46ab2c8a83d3c1b8c5bc
brSaif <saifeddine.benromdhane@protonmail.com>
2022-01-21 00:19:27 +0100

    Setup mock controller

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandsService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+
+    }
+
+    [HttpPost]
+    public ActionResult TestInBoundConnection(){
+        Console.WriteLine("---> Inbound Post # Command Service");
+        return Ok("Inbound test from platform controller");
+    }
+}
