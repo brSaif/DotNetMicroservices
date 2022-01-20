eb5bc3afa4ada82716803ab65f529fd89e1af623
brSaif <saifeddine.benromdhane@protonmail.com>
2022-01-21 00:09:00 +0100

    Setup platforms-service app Persistance

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform is null) throw new ArgumentNullException(nameof(platform));
+            _dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _dbContext.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id) => _dbContext.Platforms.FirstOrDefault(c => c.Id == id);
+
+        public bool SaveChanges()
+        {
+            return (_dbContext.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(WebApplication app)
+        {
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                SeedData(context: serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("---> Seeding Data");
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name = "Dotnet  ", Publisher = "MS", Cost = "Free"},
+                    new Models.Platform() { Name = "Dotnet 1", Publisher = "MS", Cost = "Free"},
+                    new Models.Platform() { Name = "Dotnet 2", Publisher = "MS", Cost = "Free"}
+                    );
+                context.SaveChanges();
+            }
+            else
+            {
+                 Console.WriteLine("---> We already have data");
+            }
+        }
+    }
+}
