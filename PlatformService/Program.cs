616d05cb32c0e473501af1b4382ddd5cc0065950
brSaif <saifeddine.benromdhane@protonmail.com>
2022-01-21 00:05:33 +0100

    Initial commit

---
+++ PlatformService/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+if(builder.Environment.IsProduction()){
+    Console.WriteLine("---> Using mssql db");
+    builder.Services.AddDbContext<AppDbContext>(opt 
+        => opt.UseSqlServer());
+}else{
+    Console.WriteLine("---> using inMem db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseInMemoryDatabase("InMem"));
+}
+builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+
+builder.Services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var srt = builder.Configuration.GetValue<string>("CommandService");
+Console.WriteLine($"---> EndPoint : {srt}");
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+PrepDb.PrepPopulation(app);
+
+app.Run();
